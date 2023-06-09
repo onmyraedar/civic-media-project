7e046900b30b104f1ad6495c0b75033edba0d61a
Reanna Ishmael <rishmael@fordham.edu>
2023-06-08 23:05:50 -0400

    Add styles to exercise page sidebar

--- src/components/ExercisePage.js
+++ src/components/ExercisePage.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import Tab from 'react-bootstrap/Tab';
+
+import { LayoutTextSidebarReverse, PersonVideo, PlayBtn } from "react-bootstrap-icons";
+
+import "./ExercisePage.css";
 
 export default function ExercisePage() {
 
@@ -93,6 +99,16 @@ export default function ExercisePage() {
     ]
   }
 
+  function getIcon(item) {
+    if (item.type === "video") {
+      return <PlayBtn />
+    } else if (item.type === "contact") {
+      return <PersonVideo />
+    } else {
+      return <LayoutTextSidebarReverse />
+    }
+  }
+
   function handleClickLearn() {
     setMode("learn");
     setActiveTabKey("#learn1");
@@ -104,48 +120,72 @@ export default function ExercisePage() {
   }
 
   return (
-    <Tab.Container 
-      className="flex-grow-1 d-flex flex-column"
-      id="exercise-content-tabs"
-      activeKey={activeTabKey}
-      onSelect={(k) => setActiveTabKey(k)}
-    >
-      <Row className="flex-grow-1">
-        <Col lg={4} className="d-flex flex-column p-4 light-gray-bckgd">
-          <ListGroup>
-            {mode === "learn" ? 
-            exercise.learnContent.map((item) => (
-              <ListGroup.Item action href={`#${item.id}`}>
-                {item.menuLabel}
-              </ListGroup.Item>
-            ))
-            : exercise.teachContent.map((item) => (
-              <ListGroup.Item action href={`#${item.id}`}>
-                {item.menuLabel}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-          <button onClick={handleClickLearn}>Learn</button>
-          <button onClick={handleClickTeach}>Teach</button>
-        </Col>
-        <Col lg={8} className="p-4">
-          <Tab.Content>
-            {mode === "learn" ? 
-            exercise.learnContent.map((item) => (
-              <Tab.Pane eventKey={`#${item.id}`}>
-                <h4>Exercise {exerciseId} content goes here...</h4>
-                <h6>{item.menuLabel}</h6>
-              </Tab.Pane>
-            ))
-            : exercise.teachContent.map((item) => (
-              <Tab.Pane eventKey={`#${item.id}`}>
-                <h4>Exercise {exerciseId} content goes here...</h4>
-                <h6>{item.menuLabel}</h6>
-              </Tab.Pane>
-            ))}
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+    <Container fluid className="flex-grow-1 d-flex flex-column">
+      <Tab.Container 
+        id="exercise-content-tabs"
+        activeKey={activeTabKey}
+        onSelect={(k) => setActiveTabKey(k)}
+      >
+        <Row className="flex-grow-1">
+          <Col md={4} className="d-flex flex-column p-4 light-gray-bckgd">
+            <Card className="sidebar">
+              <Card.Header className="sidebar-header">{ exercise.title }</Card.Header>
+              <ListGroup variant="flush">
+                {mode === "learn" ? 
+                exercise.learnContent.map((item) => (
+                  <ListGroup.Item action href={`#${item.id}`} key={`#${item.id}`}>
+                    {getIcon(item)}
+                    <div className="sidebar-label-container">
+                      <div className="sidebar-menu-label">{item.menuLabel}</div>
+                      {item.subLabel}
+                    </div>
+                  </ListGroup.Item>
+                ))
+                : exercise.teachContent.map((item) => (
+                  <ListGroup.Item action href={`#${item.id}`} key={`#${item.id}`}>
+                    {getIcon(item)}
+                    <div className="sidebar-label-container">
+                      <div className="sidebar-menu-label">{item.menuLabel}</div>
+                      {item.subLabel}
+                    </div>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+            <div className="d-flex px-3">
+              <button 
+                className={`learn-btn flex-grow-1 py-2 small ${mode === "learn" && "btn-active"} `}
+                onClick={handleClickLearn}
+              >
+                LEARN
+              </button>
+              <button 
+                className={`teach-btn flex-grow-1 py-2 small ${mode === "teach" && "btn-active"}`} 
+                onClick={handleClickTeach}
+              >
+                TEACH
+              </button>
+            </div>
+          </Col>
+          <Col md={8} className="p-4">
+            <Tab.Content>
+              {mode === "learn" ? 
+              exercise.learnContent.map((item) => (
+                <Tab.Pane eventKey={`#${item.id}`} key={`#${item.id}`}>
+                  <h4>Exercise {exerciseId} content goes here...</h4>
+                  <h6>{item.menuLabel}</h6>
+                </Tab.Pane>
+              ))
+              : exercise.teachContent.map((item) => (
+                <Tab.Pane eventKey={`#${item.id}`} key={`#${item.id}`}>
+                  <h4>Exercise {exerciseId} content goes here...</h4>
+                  <h6>{item.menuLabel}</h6>
+                </Tab.Pane>
+              ))}
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </Container>
   )
 }
