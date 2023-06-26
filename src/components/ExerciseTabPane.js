61766da5daddffaae4671b2100e36818ff8a568c
Reanna Ishmael <rishmael@fordham.edu>
2023-06-26 17:48:02 -0400

    Add copy to clipboard button for exercise content
    Exercise content items that have "enableCopy": true in civicPrototype.json will have a copy button on their content page.
    
    This button will allow users to copy the rich text contents of the ReactMarkdown component to their clipboard. Currently, copying images is not supported.
    
    Sample data has been added to the Motion Capture teach content section to allow for testing of the copy button.

--- src/components/ExerciseTabPane.js
+++ src/components/ExerciseTabPane.js
@@ -1,12 +1,71 @@
+import { useEffect, useRef, useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Overlay from "react-bootstrap/Overlay";
 import Tab from "react-bootstrap/Tab";
+import Tooltip from "react-bootstrap/Tooltip";
 
 import ReactMarkdown from "react-markdown";
 import ReactPlayer from "react-player";
 import remarkGfm from "remark-gfm";
 
+import { Clipboard } from "react-bootstrap-icons";
+
 import "./ExerciseTabPane.css";
 
 export default function ExerciseTabPane({ item }) {
+
+  const [showCopied, setShowCopied] = useState(false);
+
+  const exerciseTextRef = useRef(null);
+  const overlayTargetRef = useRef(null);
+
+  useEffect(() => {
+
+    if (!showCopied) {
+      return;
+    }
+
+    function onTimeout() {
+      setShowCopied(false);
+    }
+
+    // Hide the "Copied!" tooltip after 600 ms
+    const timeoutId = setTimeout(onTimeout, 600);  
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showCopied]);
+
+  async function handleCopyToClipboard() {
+
+    try {
+      // Removes <img> tags from HTML before copying to clipboard
+      const htmlWithoutImgs = exerciseTextRef.current.innerHTML.replace(/<img[^>]*>/g, "");
+
+      const clipboardItem = [new ClipboardItem({
+        "text/plain": new Blob(
+          [exerciseTextRef.current.innerText],
+          { type: "text/plain" }
+        ),
+        "text/html": new Blob(
+          [htmlWithoutImgs],
+          { type: "text/html" }
+        ),
+      })];
+    
+      await navigator.clipboard.write(clipboardItem);
+
+      // Toggle the "Copied! tooltip"
+      setShowCopied(true);
+
+    } catch (err) {
+      console.error(err.name, err.message);
+    }
+  }
+
   return(
     <Tab.Pane eventKey={`#${item.id}`}>
       {item.type === "video" &&
@@ -21,11 +80,36 @@ export default function ExerciseTabPane({ item }) {
         </div>
       }
       <h2>{item.menuLabel}</h2>
-      <ReactMarkdown 
-        children={item.text}
-        linkTarget="_blank"
-        remarkPlugins={[remarkGfm]} 
-      />
+      <div ref={exerciseTextRef}>
+        <ReactMarkdown 
+          children={item.text}
+          components={{
+            img: ({node, ...props}) => <Image fluid {...props} />
+          }}
+          linkTarget="_blank"
+          remarkPlugins={[remarkGfm]} 
+        />
+      </div>
+      {item.enableCopy &&
+      <>
+        <Button
+          ref={overlayTargetRef}
+          onClick={handleCopyToClipboard}
+          variant="outline-secondary"
+          aria-label="Copy"
+        >
+          <Clipboard /><span className="ms-2">Copy</span>
+        </Button>
+        <Overlay 
+          target={overlayTargetRef.current}
+          show={showCopied}
+          placement="right"
+        >
+          {(props) => (
+            <Tooltip {...props}>Copied!</Tooltip>
+          )}
+        </Overlay>
+      </>}
     </Tab.Pane>
   );
 }
